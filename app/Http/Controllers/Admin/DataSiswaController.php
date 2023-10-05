34db3f092ded92c8128a6be90d845429c9a538c1
efronpaduansi <eufrondpaduansi@gmail.com>
2023-10-05 21:45:24 +0700

    Export excel feature for data siswa

--- app/Http/Controllers/Admin/DataSiswaController.php
+++ app/Http/Controllers/Admin/DataSiswaController.php
@@ -2,11 +2,13 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
-use Illuminate\Http\Request;
 use App\Models\Siswa;
 use App\Models\Berkas;
 use App\Models\Website;
+use App\Exports\SiswaExport;
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+use Maatwebsite\Excel\Facades\Excel;
 
 class DataSiswaController extends Controller
 {
@@ -29,5 +31,11 @@ class DataSiswaController extends Controller
         $siswa = Siswa::find($id);
         $siswa->delete();
         return redirect()->route('admin.siswa.index')->with('success', 'Data siswa berhasil dihapus');
+    }
+
+    //export data siswa to excel file
+    public function exportExcel()
+    {
+        return Excel::download(new SiswaExport, 'data_siswa.xlsx');
     }
 }
