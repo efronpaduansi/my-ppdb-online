62f9125e7c62294d00df8efee3d4f634b0fc2160
efronpaduansi <eufrondpaduansi@gmail.com>
2023-10-19 17:47:26 +0700

    update soal random

--- app/Http/Controllers/Guest/UjianController.php
+++ app/Http/Controllers/Guest/UjianController.php
@@ -12,7 +12,15 @@ class UjianController extends Controller
 
     public function index()
     {
-        $soals = BankSoal::inRandomOrder()->get();
+        // $soals = BankSoal::inRandomOrder()->get();
+
+        if (session()->has('soals')) {
+            $soals = session('soals');
+        } else {
+            $soals = BankSoal::inRandomOrder()->get();
+            session(['soals' => $soals]);
+        }
+
         return view('backend.guest.ujian.index', compact('soals'));
     }
 }
