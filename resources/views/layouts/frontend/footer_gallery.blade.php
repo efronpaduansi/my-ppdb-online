568bd79cfb78e55769258c137744a5aeb9cac730
efronpaduansi <eufrondpaduansi@gmail.com>
2023-10-19 16:42:10 +0700

    show image

--- resources/views/layouts/frontend/footer_gallery.blade.php
+++ resources/views/layouts/frontend/footer_gallery.blade.php
@@ -1,8 +1,14 @@
 <ul class="flickr-photos">
     @foreach ($galleries as $gallery)
-        <li>
+        {{-- <li>
             <a href="#"><img class="img-responsive" src="{{ asset('uploads/frontend/' . $gallery->image_path) }}"
                     alt="flickr"></a>
-        </li>
+        </li> --}}
+        <div class="col-md-4">
+            <a href="{{ asset('uploads/frontend/' . $gallery->image_path) }}" data-lightbox="image">
+                <img src="{{ asset('uploads/frontend/' . $gallery->image_path) }}" alt="Bronze Time Hotel"
+                    height="200px" />
+            </a>
+        </div>
     @endforeach
 </ul>
