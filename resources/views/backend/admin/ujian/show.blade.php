ea83b91d82b0c3c7274f6805fe5f9117b9cf2645
efronpaduansi <eufrondpaduansi@gmail.com>
2023-11-05 21:31:04 +0700

    update table reponsive

--- resources/views/backend/admin/ujian/show.blade.php
+++ resources/views/backend/admin/ujian/show.blade.php
@@ -10,22 +10,24 @@
             <div class="ibox-title">Hasil Seleksi Calon Siswa</div>
         </div>
         <div class="ibox-body">
-            <table class="table table-striped table-bordered table-hover" id="example-table" cellspacing="0" width="100%">
-                <tbody>
-                    <tr>
-                        <th>Nama Calon Siswa </th>
-                        <td>{{ $user->name }}</td>
-                    </tr>
-                    <tr>
-                        <th>Jumlah Benar </th>
-                        <td>{{ $jmlBenar }} dari {{ $jmlSoal }} soal</td>
-                    </tr>
-                    <tr>
-                        <th>Nilai </th>
-                        <td>{{ number_format($nilai, 2, '.', '.') }}</td>
-                    </tr>
-                </tbody>
-            </table>
+            <div class="table-responsive">
+                <table class="table table-striped table-bordered table-hover" id="example-table" cellspacing="0" width="100%">
+                    <tbody>
+                        <tr>
+                            <th>Nama Calon Siswa </th>
+                            <td>{{ $user->name }}</td>
+                        </tr>
+                        <tr>
+                            <th>Jumlah Benar </th>
+                            <td>{{ $jmlBenar }} dari {{ $jmlSoal }} soal</td>
+                        </tr>
+                        <tr>
+                            <th>Nilai </th>
+                            <td>{{ number_format($nilai, 2, '.', '.') }}</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </div>
             <a href="{{ route('admin.ujian') }}" class="btn btn-primary mt-3">Kembali</a>
         </div>
     </div>
