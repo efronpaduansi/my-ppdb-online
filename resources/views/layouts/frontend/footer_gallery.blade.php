a3565dd3c9fc4ea9ed7cbffa86f2c153761f8b85
efronpaduansi <eufrondpaduansi@gmail.com>
2024-01-06 08:43:46 +0700

    update footer gallery

--- resources/views/layouts/frontend/footer_gallery.blade.php
+++ resources/views/layouts/frontend/footer_gallery.blade.php
@@ -1,6 +1,6 @@
 <ul class="flickr-photos">
     @foreach ($galleries as $gallery)
-        <div class="col-md-1 justify-content-center text-center">
+        <div class="col-md-3 justify-content-center text-center">
             <a href="{{ asset('uploads/frontend/' . $gallery->image_path) }}" data-lightbox="image">
                 <img src="{{ asset('uploads/frontend/' . $gallery->image_path) }}" alt="PPDB Online"
                     height="200px" />
